8392a3b20854ce3ca90226f24a13e30ac903f5a3
codeMauguin <2508192786@qq.com>
2022-03-01 16:07:11 +0800

    12

--- Mustache/mustache.js
+++ Mustache/mustache.js
@@ -1,13 +1,14 @@
 //Todo 类型会被转换为string 使用模版解析时
-import {render} from "./index.js";
-import {isMustache} from "../util/index.js";
+import { render } from "./index.js";
+import { isMustache } from "../util/index.js";
 
 /**
  * @param {string} stencil
  * @param {any} view
  */
-export default function mustache(stencil, view) {
-    return render(ProcessingText(stencil), view);
+export default function mustache ( stencil, view )
+{
+    return render( ProcessingText( stencil ), view );
 
 }
 
@@ -16,32 +17,39 @@ export default function mustache(stencil, view) {
  * @param {any} view
  * @returns {string|boolean|Function|object}
  */
-export function propsMustache(stencil, view) {
-    return isMustache(stencil) ? mustache(stencil, view) : render(stencil, view);
+export function propsMustache ( stencil, view )
+{
+    return isMustache( stencil ) ? mustache( stencil, view ) : render( stencil, view );
 }
 
 /**
  * @param {string} text
  * @returns {string}
  */
-export function ProcessingText(text) {
-    const stencilRegexp = /{{([\w\W]+)}}|\${([\w\W]+)}/g;
+export function ProcessingText ( text )
+{
+    const stencilRegexp = /{{(.+?)}}|\${(.+?)}/g;
     let match;
     let stencil = [];
     let index = 0;
-    if ((match = stencilRegexp.exec(text)) !== null) {
-        do {
+    if ( ( match = stencilRegexp.exec( text ) ) !== null )
+    {
+        do
+        {
             // s l
             let lastIndex = stencilRegexp.lastIndex;
-            if (match.index > index) {
-                stencil.push(`\`${text.slice(index, match.index)}\``);
+            if ( match.index > index )
+            {
+                stencil.push( `\`${ text.slice( index, match.index ) }\`` );
             }
-            stencil.push(`(${match[1] ?? match[2]})`)
+            stencil.push( `(${ match[ 1 ] ?? match[ 2 ] })` );
             index = lastIndex;
-        } while ((match = stencilRegexp.exec(text)) !== null);
-        if (index < text.length) {
-            stencil.push(`\`${text.slice(index, text.length)}\``);
+        } while ( ( match = stencilRegexp.exec( text ) ) !== null );
+        if ( index < text.length )
+        {
+            stencil.push( `\`${ text.slice( index, text.length ) }\`` );
         }
-        return stencil.join("+");
+        return stencil.join( "+" );
     } else return text;
 }
+
