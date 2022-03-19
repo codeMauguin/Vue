8e536ab9087c25b21b0cc1397ff080666201ce57
陈浩 <2508192786@qq.com>
2022-03-19 22:21:47 +0800

    .

--- util/clone.js
+++ util/clone.js
@@ -1,35 +1,36 @@
-import { isArray, isNull, clone as h, isNotObject } from "./index.js";
-import { isFunction } from "./Type.js";
+import {clone as h, isArray, isNotObject, isNull} from "./index.js";
+import {isFunction} from "./Type.js";
 
-export default function ( target )
-{
-    if ( isNull( target ) )
-    {
-        console.warn( "target is null" );
+export default function (target) {
+    if (isNull(target)) {
+        console.warn("target is null");
     }
-    if ( isNotObject( target ) || isFunction( target ) )
-    {
+    if (isNotObject(target) || isFunction(target)) {
         return target;
     }
-    if ( isArray( target ) )
-    {
+    if (isArray(target)) {
         const clone = [];
-        Array.from( target ).forEach( r =>
-        {
-            clone.push( h( r ) );
-        } );
+        Array.from(target)
+             .forEach(r => {
+                 clone.push(h(r));
+             });
         return clone;
-    } else
-    {
-        const clone = {};
-        const keys = Reflect.ownKeys( target );
-        keys.forEach( key =>
-        {
-            Reflect.set( clone, key, h(
-                Reflect.get( target, key, target )
-            ), clone );
-        } );
-        
+    } else {
+        const clone = Object.create({
+                                        __proto__: target.__proto__
+                                    });
+        const keys = Reflect.ownKeys(target);
+        for (const key of keys) {
+            Reflect.set(clone,
+                        key,
+                        h(
+                            Reflect.get(target,
+                                        key,
+                                        target)
+                        ),
+                        clone);
+        }
+
         return clone;
     }
 
